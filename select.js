4cd7a783cd405a6c952f505677f08480578a5538
Tobias Bengfort <tobias.bengfort@posteo.de>
2021-03-25 09:30:00 +0100

    refresh list on every open

--- select.js
+++ select.js
@@ -14,18 +14,11 @@ class Select {
 		this.id = id;
 		this.original = original;
 		this.focus = -1;
+		this.indexMap = [];
 
 		this.createElements();
 		original.hidden = true;
 		original.after(this.wrapper);
-	}
-
-	forOption(fn) {
-		Array.from(this.original.options).forEach(fn);
-	}
-
-	focussed() {
-		return this.ul.children[this.focus];
 	}
 
 	createElements() {
@@ -40,7 +33,6 @@ class Select {
 		this.wrapper.setAttribute('aria-expanded', 'false');
 		this.wrapper.setAttribute('aria-has-popup', 'listbox');
 		this.input.setAttribute('aria-autocomplete', 'list');
-		this.input.setAttribute('aria-activedescendant', 'TODO');
 		this.ul.setAttribute('role', 'listbox');
 		this.ul.setAttribute('aria-labelledby', 'TODO');
 		this.ul.tabIndex = -1;
@@ -49,80 +41,69 @@ class Select {
 		this.input.oninput = this.oninput.bind(this);
 		this.input.onblur = this.onblur.bind(this);
 		this.input.onclick = () => {
-			this.focus = this.focus === -1 ? 0 : -1;
-			this.update();
-		};
-
-		this.forOption((op, i) => {
-			var li = document.createElement('li');
-			li.id = this.id + '_option_' + i;
-			li.textContent = op.label;
-			li.onclick = () => {
-				this.setValue(i);
-				this.input.focus();
+			if (this.focus === -1) {
+				this.open();
+			} else {
+				this.close();
 			}
-			this.ul.append(li);
-		});
+		};
 	}
 
 	update() {
-		var q = this.input.value.toLowerCase();
-		this.forOption((op, i) => {
-			var li = this.ul.children[i];
-			li.hidden = this.input.value && li.textContent.toLowerCase().indexOf(q) === -1;
-			li.classList.remove('select--has-focus');
-		});
-
-		if (this.focus !== -1) {
-			if (this.focussed().hidden) {
-				this.focus = 0;
-				while (this.focussed() && this.focussed().hidden) {
-					this.focus += 1;
-				}
-			}
-			if (this.focussed()) {
-				this.focussed().classList.add('select--has-focus');
-				this.focussed().scrollIntoView({block: 'nearest'});
-			}
-		}
-
-		this.wrapper.setAttribute('aria-expanded', this.focus !== -1);
-		if (this.focussed()) {
-			this.input.setAttribute('aria-activedescendant', this.id + '_option_' + this.focus);
+		if (this.focus !== -1 && this.ul.children.length) {
+			Array.from(this.ul.children).forEach((li, i) => {
+				li.classList.toggle('select--has-focus', i === this.focus);
+			});
+			this.wrapper.setAttribute('aria-expanded', 'true');
+			this.input.setAttribute('aria-activedescendant', this.id + '_option_' + this.indexMap[this.focus]);
 		} else {
+			this.wrapper.setAttribute('aria-expanded', 'false');
 			this.input.setAttribute('aria-activedescendant', '');
 		}
 	}
 
-	moveFocus(k) {
-		var step = 1;
-		if (k < 0) {
-			k = -k;
-			step = -1;
-		}
+	open() {
+		var q = this.input.value.toLowerCase();
+		this.focus = 0;
+		this.ul.innerHTML = '';
+		this.indexMap = [];
+		Array.from(this.original.options).forEach((op, i) => {
+			if (op.label.toLowerCase().indexOf(q) !== -1) {
+				var li = document.createElement('li');
+				li.id = this.id + '_option_' + i;
+				li.textContent = op.label;
+				li.onclick = () => {
+					this.setValue(i);
+					this.input.focus();
+				};
+				this.ul.append(li);
+				this.indexMap.push(i);
+			}
+		});
+		this.update();
+	}
 
-		var focus = this.focus;
-		var li;
-		for (var i = 0; i < k; i++) {
-			focus += step;
-			li = this.ul.children[focus];
-			while (li && li.hidden) {
-				focus += step;
-				li = this.ul.children[focus];
-			}
-			if (li) {
-				this.focus = focus;
-			}
-		}
+	close() {
+		this.focus = -1;
+		this.ul.innerHTML = '';
+		this.indexMap = [];
+		this.update();
+	}
+
+	moveFocus(k) {
+		this.focus += k;
+		this.focus = Math.max(this.focus, 0);
+		this.focus = Math.min(this.focus, this.ul.children.length - 1);
+		this.ul.children[this.focus].scrollIntoView({block: 'nearest'});
 		this.update();
 	}
 
 	setValue(i) {
-		this.input.value = this.ul.children[i].textContent;
+		this.input.value = this.original.options[i].label;
 		this.input.setCustomValidity('');
 		this.original.options[i].selected = true;
 		this.original.dispatchEvent(new Event('change'));
-		this.focus = -1;
+		this.close();
 		this.update();
 	}
 
@@ -142,40 +123,37 @@ class Select {
 				this.moveFocus(-10);
 			} else if (event.keyCode === KEYS.ENTER) {
 				event.preventDefault();
-				this.setValue(this.focus);
+				this.setValue(this.indexMap[this.focus]);
 			} else if (
 				event.keyCode === KEYS.ESC
 				|| event.keyCode === KEYS.LEFT
 				|| event.keyCode === KEYS.RIGHT
 			) {
-				this.focus = -1;
-				this.update();
+				this.close();
 			}
 		} else {
 			if (event.keyCode === KEYS.DOWN) {
 				event.preventDefault();
-				this.focus = 0;
-				this.update();
+				this.open();
 			}
 		}
 	}
 
 	oninput(event) {
-		this.focus = 0;
-		this.update();
+		this.open();
 	}
 
 	onblur(event) {
 		if (event.relatedTarget !== this.ul) {
-			if (this.focussed()) {
-				this.setValue(this.focus);
+			if (this.indexMap.length) {
+				this.setValue(this.indexMap[this.focus]);
 			}
-
 			if (this.input.value === this.original.selectedOptions[0].label) {
 				this.input.setCustomValidity('');
 			} else {
 				this.input.setCustomValidity('invalid choice');
 			}
+			this.close();
 		}
 	}
 }
